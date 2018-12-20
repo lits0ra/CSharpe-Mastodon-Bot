3b096352d81895d79aec1c0fe0a70a6dc1eee170
s0ralit <s0ralit@protonmail.com>
2018-12-21 01:36:12 +0900

    とりあえず完成した

--- Mastodon/Program.cs
+++ Mastodon/Program.cs
@@ -1,11 +1,43 @@
 ﻿using Mastonet;
-namespace Mastodon
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoraMastodon
 {
-    class MainClass
+    class Mastodon
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            var authClient = new AuthenticationClient("mstdn.jp");
+            var mastodon = new Mastodon();
+            mastodon.Streaming();
+
+            Console.ReadKey();
+        }
+
+        private async Task Streaming()
+        {
+            var authClient = new AuthenticationClient("pawoo.net");
+            var appRegistration = await authClient.CreateApp("s0ratest", Scope.Read | Scope.Write | Scope.Follow);
+
+            var auth = await authClient.ConnectWithPassword("Email", "Password");
+
+            var client = new MastodonClient(appRegistration, auth);
+
+            var streaming = client.GetPublicStreaming();
+            streaming.OnUpdate += (sender, e) =>
+            {
+                Console.WriteLine(e.Status.Content);
+;               Console.WriteLine(e.Status.Id);
+                Console.WriteLine(e.Status.Language);
+                Console.WriteLine(e.Status.FavouritesCount);
+                Console.WriteLine(e.Status.CreatedAt);
+                Console.WriteLine();
+            };
+
+            await streaming.Start();
         }
     }
 }
